68eea8ab040e63b0c68e44f72ef7873165873518
Eduardiko <emb2304@yahoo.es>
2022-04-15 09:51:27 +0200

    Implemented Zhib w/Idle & Walk anims

--- Assets/Scripts/characterwalkingscript.cs
+++ Assets/Scripts/characterwalkingscript.cs
@@ -8,10 +8,13 @@ public class characterwalkingscript : MonoBehaviour
 
     public NavMeshAgent playerAgent;
     public Camera playerCamera;
+
+    private Animator zhibAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        zhibAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,9 +31,32 @@ public class characterwalkingscript : MonoBehaviour
                 if(meshHit.collider.tag == "Floor")
                 {
                     playerAgent.SetDestination(meshHit.point);
+                    
+                    if (zhibAnimator != null)
+                    {
+                        zhibAnimator.SetTrigger("isWalking");
+                    }
                 }
             }
         }
         
+        if(playerAgent.remainingDistance <= playerAgent.stoppingDistance)
+        {
+            if (zhibAnimator != null)
+            {
+                zhibAnimator.SetTrigger("hasStopped");
+            }
+        }
+
+
+        //if(playerAgent.velocity != Vector3.zero)
+        //{
+        //    if(zhibAnimator != null)
+        //    {
+        //        zhibAnimator.SetTrigger("isWalking");
+        //    }
+        //} else if (playerAgent.velocity == Vector3.zero)
+        //{
+        //}
     }
 }
